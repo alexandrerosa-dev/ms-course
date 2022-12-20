2668f9534b7229700047570758bb9e6689ba6527
alexandrerosa-dev <asr.alexandrerosa@gmail.com>
2022-12-20 10:45:43 -0300

    Login, JWT token

--- hr-oauth/src/main/java/com/devsuperior/hroauth/config/AppConfig.java
+++ hr-oauth/src/main/java/com/devsuperior/hroauth/config/AppConfig.java
@@ -1,14 +1,37 @@
 package com.devsuperior.hroauth.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+@Configuration
 @EnableWebSecurity
 public class AppConfig extends WebSecurityConfigurerAdapter {
 
+	@Bean
+	public BCryptPasswordEncoder BCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter  tokenConverter = new JwtAccessTokenConverter();
+		tokenConverter.setSigningKey("MY-SECRET-KEY");
+		return tokenConverter;
+	}
+	
+	@Bean
+	public JwtTokenStore tokenStore() {
+		return new JwtTokenStore(accessTokenConverter());
+	}
+	
+	
 	@Override
-
 	public void configure(HttpSecurity http) throws Exception {
 
 		// @formatter:off
@@ -34,4 +57,5 @@ public class AppConfig extends WebSecurityConfigurerAdapter {
 	    // @formatter:on
 
 	}
+
 }
